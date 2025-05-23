b93c7bd81b8999ace4cbf0f3e2634d914366f419
Malo Virlogeux <malo.virlogeux@epitech.eu>
2025-05-23 19:07:41 +0200

    feat: add lucide-react dependency and update layout and footer components

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,44 @@
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto text-center">
-            <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
-            <p>
-            <a href="/about" className="text-gray-400 hover:text-white">About</a> |{" "}
-            <a href="/contact" className="text-gray-400 hover:text-white">Contact</a> |{" "}
-            <a href="/projects" className="text-gray-400 hover:text-white">Projects</a>
-            </p>
-        </div>
+        <footer className="font-clash">
+            <span className="flex justify-center text-center text-[12vw] font-bold text-black-400 px-10">
+                enzobeystudio*
+            </span>
+
+            <div
+                className="flex flex-col items-center justify-center gap-4
+                      px-10 sm:flex-row sm:justify-between text-lg font-medium"
+            >
+                {[
+                    {
+                        href: "https://www.instagram.com/enzobeystudio/",
+                        label: "INSTAGRAM",
+                    },
+                    {
+                        href: "https://www.linkedin.com/in/enzo-bey-0b74bb2b6/",
+                        label: "LINKEDIN",
+                    },
+                    {
+                        href: "https://www.behance.net/enzo_bey",
+                        label: "BEHANCE",
+                    },
+                    { href: "/", label: "UNSPLASH" },
+                ].map(({ href, label }) => (
+                    <Link
+                        key={href}
+                        href={href}
+                        className="group text-black-400 transition-colors hover:scale-105 hover:text-black-200"
+                    >
+                        <span className="flex items-center gap-1">
+                            {label}
+                            <ArrowUpRight className="inline-block group-hover:animate-bounce" />
+                        </span>
+                    </Link>
+                ))}
+            </div>
         </footer>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-roboto)", "system-ui", "sans-serif"],
+                clash: ["var(--font-clash)"],
+            },
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
+    variants: {
+        extend: {
+            animation: ["hover", "group-hover"],
+        },
+    },
 } satisfies Config;
