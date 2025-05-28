3c7abeb03ca3047ccbd6ae682a4dac8f1e7a99df
Malo Virlogeux <malo.virlogeux@epitech.eu>
2025-05-28 14:46:04 +0200

    style: refactor Header component layout and styling for improved structure

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,14 +2,31 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="text-white py-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <nav>
-                    <ul className="flex space-x-4">
-                        <li><Link href="/about" className="text-gray-400 hover:text-white">About</Link></li>
-                        <li><Link href="/contact" className="text-gray-400 hover:text-white">Contact</Link></li>
-                    </ul>
-                </nav>
+        <header className="text-black px-10 pt-10">
+            <div className="grid grid-cols-3 grid-rows-2 gap-0">
+                <div className="col-start-1 col-end-2 row-start-1 row-end-2">
+                    <div className="flex flex-col text-[9px]">
+                        <Link href="/">
+                            <span className="uppercase font-bold">
+                                bey enzo
+                            </span>
+                        </Link>
+                        <span className="uppercase">graphic design</span>
+                        <span className="uppercase">& photographie</span>
+                    </div>
+                </div>
+                <div className="col-start-2 col-end-3 row-start-1 row-end-2"></div>
+                <div className="col-start-3 col-end-4 row-start-1 row-end-2">
+                    <div className="flex gap-4 text-[9px] uppercase font-medium">
+                        <Link href="/about">
+                            <span>about</span>
+                        </Link>
+                        <Link href="/contact">
+                            <span>contact</span>
+                        </Link>
+                    </div>
+                </div>
+                <div className="col-start-1 col-end-4 row-start-2 row-end-3"></div>
             </div>
         </header>
     );
