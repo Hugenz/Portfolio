4b93b640420b592ff603fe98b2196b7a9ebabf05
Malo Virlogeux <malo.virlogeux@epitech.eu>
2025-05-28 11:33:27 +0200

    style: update About and Footer components for improved layout and styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,19 @@
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>This is the about page of the application.</p>
+        <div className="flex px-20">
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Debitis doloribus consectetur voluptates nihil autem repellendus
+                atque, corporis facilis reprehenderit, modi recusandae suscipit,
+                sit hic dolorum quos error quidem nemo ipsa.
+            </p>
+            // IMage
+            <img
+                src="https://images.unsplash.com/photo-1677631950982-0f1a3b5c4d7e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+                alt="About"
+                className="w-full h-96 object-cover rounded-lg shadow-lg"
+            />
+            <span></span>
         </div>
     );
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,13 +4,13 @@ import { ArrowUpRight } from "lucide-react";
 export default function Footer() {
     return (
         <footer className="font-clash px-10">
-            <span className="flex text-[11.5vw] font-bold text-black-400 mb-[-1rem]">
+            <span className="text-[10.5vw] md:text-[11.5vw] font-bold text-black-400 mb-[-1rem] justify-center">
                 enzobeystudio*
             </span>
 
             <div
-                className="flex flex-col items-center justify-center gap-4
-                     sm:flex-row sm:justify-between text-lg font-medium"
+                className="flex flex-col items-center justify-center gap-4 pt-6 md:pt-0
+                     md:flex-row sm:justify-between text-lg font-medium"
             >
                 {[
                     {
@@ -30,12 +30,10 @@ export default function Footer() {
                     <Link
                         key={href}
                         href={href}
-                        className="group text-black-400 transition-colors hover:scale-105 hover:text-black-200"
+                        className="flex justify-between w-full md:flex md:justify-center md:items-center md:w-auto group text-black-400 transition-colors hover:scale-105 hover:text-black-200"
                     >
-                        <span className="flex items-center gap-1">
-                            {label}
-                            <ArrowUpRight className="inline-block group-hover:animate-bounce" />
-                        </span>
+                        <span className="flex items-center gap-1">{label}</span>
+                        <ArrowUpRight className="inline-block group-hover:animate-bounce" />
                     </Link>
                 ))}
             </div>
